2d093936a345d38a11e45c96f03a6e2271035b19
an hong <hongan529@gmail.com>
2020-10-08 13:35:09 +0700

    test-chart

---
+++ Models/GetChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Christoc.Modules.Chart.Models
+{
+    public class GetChart
+    {
+        public string Gender { get; set; }
+        public int Amount { get; set; }
+    }
+}
